5546fa0f835ff78b15d126ebc2afe38c982886a3
AlisovTimofey <timofeyalisov@gmail.com>
2026-03-05 23:30:58 +0300

    finish code ig

--- main.cpp
+++ main.cpp
@@ -1,3 +1,31 @@
-int main()
+#include <cstddef>
+template < class T > T *merge(const T *a, size_t sa, const T *b, size_t sb, T *c)
 {
+  size_t i = 0;
+  size_t k = 0;
+
+  while (i < sa && k < sb)
+  {
+    if (a[i] < b[k])
+    {
+      c[i + k] = a[i++];
+    }
+
+    else
+    {
+      c[i + k] = b[k++];
+    }
+  }
+
+  while (i < sa)
+  {
+    c[i + k] = a[i++];
+  }
+
+  while (k < sb)
+  {
+    c[i + k] = b[k++];
+  }
+
+  return c;
 }
