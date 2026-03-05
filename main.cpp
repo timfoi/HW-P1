e591cbb609346f95306a09d48c1b3af6b6dfcebe
AlisovTimofey <timofeyalisov@gmail.com>
2026-03-05 23:38:24 +0300

    finish

--- main.cpp
+++ main.cpp
@@ -6,26 +6,27 @@ template < class T > T *merge(const T *a, size_t sa, const T *b, size_t sb, T *c
 
   while (i < sa && k < sb)
   {
-    if (a[i] < b[k])
+    if (a[i] < b[k]) // bool operator<(const T&)
     {
-      c[i + k] = a[i++];
+      c[i + k] = a[i++]; // T& operator=(const T&)
     }
-
     else
     {
-      c[i + k] = b[k++];
+      c[i + k] = b[k++]; // T& operator=(const T&)
     }
   }
 
   while (i < sa)
   {
-    c[i + k] = a[i++];
+    c[i + k] = a[i++]; // T& operator=(const T&)
   }
 
   while (k < sb)
   {
-    c[i + k] = b[k++];
+    c[i + k] = b[k++]; // T& operator=(const T&)
   }
 
   return c;
 }
+
+// недостатки: память для итогового массива должна быть выделена заранее пользователем.
